1ff05a45949c376507d7f92d690d308238039993
Michael Fatemi <myfatemi04@gmail.com>
2022-02-02 23:15:09 -0500

    fix chunk delimiter that caused program to explode

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const ws = require("ws");
-const net = require("net");
+const ws = require('ws');
+const net = require('net');
 
 const socketServer = new net.Server();
 socketServer.on('connection', sock => {
-    let partial = '';
-    sock.on('data', data => {
-        partial += data.toString();
+	console.log('Received connection from socket client');
+	let partial = '';
+	sock.on('data', data => {
+		partial += data.toString();
 
-        // Splits apart packets that arrived together
-        // This regex finds positions that are preceded by a '}' and followed by a '{'
-        let chunks = partial.split(/(?<=})(?={)/);
-        for (let chunk of chunks) {
-            try {
-                // Check if the chunk is valid JSON
-                JSON.parse(chunk);
+		// Splits apart packets that arrived together
+		// This regex finds positions that are preceded by a '}' and followed by a '{'
+		let chunks = partial.split(/(?<=})\n?(?={)/);
+		for (let chunk of chunks) {
+			try {
+				// Check if the chunk is valid JSON
+				JSON.parse(chunk);
 
-                // Broadcast the chunk
-                websocketConnectionPool.forEach(sock => sock.send(chunk));
+				// Broadcast the chunk
+				websocketConnectionPool.forEach(sock => sock.send(chunk));
 
-                // Clear the running partial chunk
-                partial = '';
-            } catch (e) {
-                // Chunk is unparseable
-                partial += chunk
-            }
-        }
-    })
+				// Clear the running partial chunk
+				partial = '';
+			} catch (e) {
+				// Chunk is unparseable
+				partial += chunk;
+			}
+		}
+	});
 });
 
 const socketServerPort = 10000;
@@ -33,21 +34,19 @@ const websocketServerPort = 20000;
 
 // Keep track of existing connections
 // Start the WebSocketServer first
-const websocketConnectionPool = new Set()
+const websocketConnectionPool = new Set();
 const websocketServer = new ws.WebSocketServer({
-    port: websocketServerPort
-})
+	port: websocketServerPort,
+});
 websocketServer.on('connection', sock => {
-    websocketConnectionPool.add(sock);
-    sock.on('close', () => {
-        websocketConnectionPool.delete(sock);
-    });
-})
-console.log("[ws] listening on", websocketServerPort);
+	console.log('Received connection from WebSocket client');
+	websocketConnectionPool.add(sock);
+	sock.on('close', () => {
+		websocketConnectionPool.delete(sock);
+	});
+});
+console.log('[ws] listening on', websocketServerPort);
 
 socketServer.listen(socketServerPort, undefined, undefined, () => {
-    console.log("[odas] listening on", socketServerPort);
+	console.log('[odas] listening on', socketServerPort);
 });
-
-
-
